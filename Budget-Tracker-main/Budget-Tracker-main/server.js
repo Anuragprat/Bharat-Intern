606a0ae082c1caa326f6ec1a3d540194a841e6bf
Anurag <anuragtivari850@gmail.com>
2024-01-09 20:43:26 +0530

    final commit

---
+++ Budget-Tracker-main/Budget-Tracker-main/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017/budget-tracker', {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+
+//  ⬢ polar-hollows-87169 https://polar-hollows-87169.herokuapp.com/ | https://git.heroku.com/polar-hollows-87169.git
